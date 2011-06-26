4515fe0ed9f656716cc29668337a944810c035d4
Nutthaka Tanthanuch <nutthaka@gmail.com>
2011-06-26 16:57:41 +1000

    adding basic java project

---
+++ src/main/java/me/nutthaka/App.java
@@ -0,0 +1,7 @@
+package me.nutthaka;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/me/nutthaka/AppTest.java
@@ -0,0 +1,14 @@
+package me.nutthaka;
+
+import org.junit.*;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.matchers.JUnitMatchers.*;
+
+public class AppTest  {
+    @Test
+    public void testApp() {
+        assertTrue(true);
+    }
+}
