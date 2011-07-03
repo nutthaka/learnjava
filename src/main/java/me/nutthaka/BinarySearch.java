03ed94a47687f35e90f7dc441aa0fc7bbe32084f
Nutthaka Tanthanuch <nutthaka@gmail.com>
2011-07-04 00:37:35 +1000

    Added Code Kata number 2 BinarySearch implementation

--- src/main/java/me/nutthaka/BinarySearch.java
+++ src/main/java/me/nutthaka/BinarySearch.java
@@ -1,7 +1,33 @@
 package me.nutthaka;
 
 public class BinarySearch {
+
     public static int chop(int target, int[] sourceArray) {
-        return -2;
+        return chop(target, sourceArray, 0, sourceArray.length);
+    }
+
+    public static int chop(int target, int[] sourceArray, int leftIndex, int length) {
+        if (length == 0) {
+            return -1;
+        }
+
+        int arrayMidPosition = leftIndex+(length/2);
+        int midValue = sourceArray[arrayMidPosition];
+
+        if (midValue == target){
+            return arrayMidPosition;
+        }
+
+        if (length == 1) {
+            return -1;
+        }
+
+        if (target < midValue){
+            return chop(target, sourceArray, leftIndex, arrayMidPosition-leftIndex);
+        }
+        else {
+            return chop(target, sourceArray, arrayMidPosition+1, sourceArray.length-arrayMidPosition-1);
+        }
+
     }
 }
